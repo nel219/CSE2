4733180ae3448190c4b1550bc07a9b406f7ff0df
nel219 <nel219@lehigh.edu>
2015-08-30 18:47:45 +0000

    updated WlcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,19 @@
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[] args) {
+      
+//  print the statement with box around it
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-n--e--l--2--1--9-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This summer, I worked at a restaurant in Easton, PA.");
+    System.out.println("I talked to Lafayette students before I talked to Lehigh students.");
+  }
+}
