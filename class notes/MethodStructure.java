ccbb663a13ffc808b682ab211f4b6b700b018cb7
nel219 <nel219@lehigh.edu>
2015-09-07 14:07:02 +0000

    updated Arithmetic.java

---
+++ class notes/MethodStructure.java
@@ -0,0 +1,29 @@
+public class MethodStructure{
+        public static void computeSpeed(){
+        }
+        public static void addInterest(){
+        }
+    public static void main(String[] args) {
+        computeSpeed();
+        addInterest();
+    }
+}
+// class{ {method 1} {method 2} {main method} }
+// code broken into smaller pieces
+// each method responsible for an action
+// only main method runned, so put other methods in main
+
+
+// can use variable in all methods by declaring variable outside
+// methods but in class
+public class MethodStructure{
+        double interest=0.1
+        
+        {method 1
+            can use variable}
+        {method 2
+            can use variable}
+    public static void main (String[] args) {
+        can use variable}
+}
+
